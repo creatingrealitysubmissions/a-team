cba86df41d001bd3e98c7c3e5c0fffdc33515813
Nathaniel Sena <nate.sena1@gmail.com>
2018-03-13 22:55:06 -0700

    added Ross' move function

--- avisualizer/src/Components/Step.js
+++ avisualizer/src/Components/Step.js
@@ -2,31 +2,39 @@ import AFRAME from 'aframe'
 import React from 'react'
 //import all movingMethods
 
-const move = (movingItem) => {
-    //get item's position
-
+const move = (movingItem, property, secondaryProperty) => {
+  // If changing position
+  if (property === "position") {
+    // Get item's position
     var currentPosition = movingItem.components.position.data
-    // console.log(currentPosition)
-
-    currentPosition.x += 0.01
-    currentPosition.y = Math.sin(Date.now())
+    console.log(currentPosition)
+    // If changing x
+    if (secondaryProperty == "x") {
+      currentPosition.x += 0.01
+      currentPosition.y = Math.sin(Date.now())
+      // If changing y
+    } else if (secondaryProperty == "y") {
+      currentPosition.y += 0.01
+      currentPosition.x += Math.sin(Date.now())
+      // If changing z
+    } else if (secondaryProperty == "z") {
+      currentPosition.z += 0.01
+      currentPosition.x = Math.sin(Date.now())
+    }
+    // Update item's position
     movingItem.setAttribute('position', currentPosition);
-
-    //set item's position to new coordinates based off of method
+  }
 }
 
 AFRAME.registerComponent('step', {
   schema: {
   },
-
   init: function () {
-
   },
-
   tick: function (t, dt) {
     var movingItems = document.querySelector('#Box')
     if(movingItems){
-        move(movingItems)
+        move(movingItems, "position", "y")
     }
   }
 });
